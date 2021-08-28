8c951102881f1d5fa41396d08940d9fd7adffa5b
chomosuke <a13323600@gmail.com>
2021-08-28 21:58:10 +1000

    wrote 3 more failure test for cardPut endpoint
    For: non existent tagId, tagId of a tag that belong to the wrong user, and a request without an email

--- server/src/__tests__/api/authenticated/cardPut.ts
+++ server/src/__tests__/api/authenticated/cardPut.ts
@@ -1,6 +1,6 @@
 import { NextFunction } from 'express';
 import { Types } from 'mongoose';
-import { CardPutRequest, CardPutResponse } from '../../../../../shared/lib/api-types';
+import { CardPutRequest, CardPutResponse } from '@porkbellypro/crm-shared';
 import { cardPut } from '../../../api/authenticated/cardPut';
 import { IAuthenticatedRouter } from '../../../api/authenticated/router';
 import {
@@ -8,6 +8,7 @@ import {
 } from '../../helpers';
 import { User } from './auth';
 import { mockRequest } from './helpers';
+import { HttpStatusError } from '../../../api/HttpStatusError';
 
 describe('cardPut unit tests', () => {
   test('Success test', async () => {
@@ -141,9 +142,226 @@ describe('cardPut unit tests', () => {
     };
     expect(res.json).toBeCalledWith(response);
   });
-/*
- *test('Fail test: bad tagId');
- *test('Fail test: tagId wrong user');
- *test('Fail test: bad request');
- */
+
+  test('Fail test: bad tagId', async () => {
+    const user = {
+      id: Types.ObjectId().toString(),
+    } as User;
+
+    const tags = [
+      {
+        id: Types.ObjectId().toString(),
+        user: Types.ObjectId(user.id),
+        label: 'haha1',
+        color: 'fff',
+      },
+      {
+        id: Types.ObjectId().toString(),
+        user: Types.ObjectId(user.id),
+        label: 'haha2',
+        color: 'fff',
+      },
+      {
+        id: Types.ObjectId().toString(),
+        user: Types.ObjectId(user.id),
+        label: 'haha3',
+        color: 'fff',
+      },
+    ];
+
+    const cardId = Types.ObjectId().toString();
+
+    const routerPartial: DeepPartial<IAuthenticatedRouter> = {
+      parent: {
+        db: {
+          startSession: mockStartSession,
+        },
+        Tags: {
+          findById: mock((id) => tags.find((t) => t.id === id)),
+        },
+        Cards: {
+          create: mock(async (obj) => {
+            const {
+              name,
+              phone,
+              email,
+              jobTitle,
+              company,
+              fields,
+            } = obj;
+            return {
+              id: cardId,
+              favorite: false,
+              user: obj.user,
+              name,
+              phone,
+              email,
+              jobTitle,
+              company,
+              image: false,
+              fields,
+              tags: obj.tags.map((id: string) => Types.ObjectId(id)),
+            };
+          }),
+        },
+      },
+    };
+    const router = routerPartial as IAuthenticatedRouter;
+
+    const request: CardPutRequest = {
+      name: 'john',
+      phone: '021',
+      email: 'bla@gmail.com',
+      jobTitle: 'person',
+      company: 'wollies',
+      fields: [
+        { key: 'height', value: '10cm' }, { key: 'weight', value: 'fat' },
+      ],
+      tags: [...tags.map((t) => t.id), Types.ObjectId().toString()],
+    };
+    const req = mockRequest({
+      body: request,
+      user,
+    });
+
+    const res = mockResponse({
+      status: mock().mockReturnThis(),
+      json: mock().mockReturnThis(),
+    });
+
+    const next = mock<NextFunction>();
+
+    await expect(cardPut.implementation.call(router, req, res, next))
+      .rejects.toStrictEqual(new HttpStatusError(400));
+
+    expect(router.parent.Tags.findById).toBeCalled();
+  });
+
+  test('Fail test: tagId wrong user', async () => {
+    const user = {
+      id: Types.ObjectId().toString(),
+    } as User;
+
+    const tags = [
+      {
+        id: Types.ObjectId().toString(),
+        user: Types.ObjectId(user.id),
+        label: 'haha1',
+        color: 'fff',
+      },
+      {
+        id: Types.ObjectId().toString(),
+        user: Types.ObjectId(user.id),
+        label: 'haha2',
+        color: 'fff',
+      },
+      {
+        id: Types.ObjectId().toString(),
+        user: Types.ObjectId(),
+        label: 'haha3',
+        color: 'fff',
+      },
+    ];
+
+    const cardId = Types.ObjectId().toString();
+
+    const routerPartial: DeepPartial<IAuthenticatedRouter> = {
+      parent: {
+        db: {
+          startSession: mockStartSession,
+        },
+        Tags: {
+          findById: mock((id) => tags.find((t) => t.id === id)),
+        },
+        Cards: {
+          create: mock(async (obj) => {
+            const {
+              name,
+              phone,
+              email,
+              jobTitle,
+              company,
+              fields,
+            } = obj;
+            return {
+              id: cardId,
+              favorite: false,
+              user: obj.user,
+              name,
+              phone,
+              email,
+              jobTitle,
+              company,
+              image: false,
+              fields,
+              tags: obj.tags.map((id: string) => Types.ObjectId(id)),
+            };
+          }),
+        },
+      },
+    };
+    const router = routerPartial as IAuthenticatedRouter;
+
+    const request: CardPutRequest = {
+      name: 'john',
+      phone: '021',
+      email: 'bla@gmail.com',
+      jobTitle: 'person',
+      company: 'wollies',
+      fields: [
+        { key: 'height', value: '10cm' }, { key: 'weight', value: 'fat' },
+      ],
+      tags: tags.map((t) => t.id),
+    };
+    const req = mockRequest({
+      body: request,
+      user,
+    });
+
+    const res = mockResponse({
+      status: mock().mockReturnThis(),
+      json: mock().mockReturnThis(),
+    });
+
+    const next = mock<NextFunction>();
+
+    await expect(cardPut.implementation.call(router, req, res, next))
+      .rejects.toStrictEqual(new HttpStatusError(401));
+
+    expect(router.parent.Tags.findById).toBeCalled();
+  });
+
+  test('Fail test: bad request - no email', async () => {
+    const user = {
+      id: Types.ObjectId().toString(),
+    } as User;
+
+    const routerPartial: DeepPartial<IAuthenticatedRouter> = {};
+    const router = routerPartial as IAuthenticatedRouter;
+
+    const request: Partial<CardPutRequest> = {
+      name: 'john',
+      phone: '021',
+      jobTitle: 'person',
+      company: 'wollies',
+      fields: [
+        { key: 'height', value: '10cm' }, { key: 'weight', value: 'fat' },
+      ],
+      tags: [],
+    };
+    const req = mockRequest({
+      body: request,
+      user,
+    });
+
+    const res = mockResponse({
+      status: mock().mockReturnThis(),
+      json: mock().mockReturnThis(),
+    });
+
+    const next = mock<NextFunction>();
+
+    await expect(cardPut.implementation.call(router, req, res, next))
+      .rejects.toStrictEqual(new HttpStatusError(400));
+  });
 });
