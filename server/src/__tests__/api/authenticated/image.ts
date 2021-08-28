b1fb5f770e2bfe3d0a1fa583659240d5ac809a7e
chomosuke <a13323600@gmail.com>
2021-08-29 03:14:36 +1000

    written 3 image endpoint fail test

--- server/src/__tests__/api/authenticated/image.ts
+++ server/src/__tests__/api/authenticated/image.ts
@@ -2,6 +2,7 @@ import { NextFunction } from 'express';
 import { Types } from 'mongoose';
 import { image } from '../../../api/authenticated/image';
 import { IAuthenticatedRouter } from '../../../api/authenticated/router';
+import { HttpStatusError } from '../../../api/HttpStatusError';
 import { DeepPartial, mock, mockResponse } from '../../helpers';
 import { User } from './auth';
 import { mockRequest } from './helpers';
@@ -51,5 +52,99 @@ describe('image unit tests', () => {
 
     expect(res.end).toBeCalledTimes(1);
     expect(res.end).toBeCalledWith(imageData);
+  });
+
+  test('Fail test: no card', async () => {
+    const cardId = Types.ObjectId();
+
+    const req = mockRequest({
+      params: {
+        cardId: cardId.toString(),
+      },
+      user: {
+        id: Types.ObjectId().toString(),
+      } as User,
+    });
+
+    const res = mockResponse();
+
+    const routerPartial: DeepPartial<IAuthenticatedRouter> = {
+      parent: {
+        Cards: {
+          findById: mock().mockResolvedValue(null),
+        },
+      },
+    };
+    const router = routerPartial as IAuthenticatedRouter;
+
+    const next = mock<NextFunction>();
+
+    await expect(image.implementation.call(router, req, res, next))
+      .rejects.toStrictEqual(new HttpStatusError(404));
+  });
+
+  test('Fail test: no image', async () => {
+    const cardId = Types.ObjectId();
+
+    const req = mockRequest({
+      params: {
+        cardId: cardId.toString(),
+      },
+      user: {
+        id: Types.ObjectId().toString(),
+      } as User,
+    });
+
+    const res = mockResponse();
+
+    const routerPartial: DeepPartial<IAuthenticatedRouter> = {
+      parent: {
+        Cards: {
+          findById: mock().mockResolvedValue({
+            user: Types.ObjectId(req.user.id),
+          }),
+        },
+      },
+    };
+    const router = routerPartial as IAuthenticatedRouter;
+
+    const next = mock<NextFunction>();
+
+    await expect(image.implementation.call(router, req, res, next))
+      .rejects.toStrictEqual(new HttpStatusError(404));
+  });
+
+  test('Fail test: wrong user', async () => {
+    const cardId = Types.ObjectId();
+
+    const req = mockRequest({
+      params: {
+        cardId: cardId.toString(),
+      },
+      user: {
+        id: Types.ObjectId().toString(),
+      } as User,
+    });
+
+    const res = mockResponse();
+
+    const imageData = 'someBuffer';
+
+    const routerPartial: DeepPartial<IAuthenticatedRouter> = {
+      parent: {
+        Cards: {
+          findById: mock().mockResolvedValue({
+            image: imageData,
+            user: Types.ObjectId(),
+          }),
+        },
+      },
+    };
+    const router = routerPartial as IAuthenticatedRouter;
+
+    const next = mock<NextFunction>();
+
+    await expect(image.implementation.call(router, req, res, next))
+      .rejects.toStrictEqual(new HttpStatusError(401));
   });
 });
