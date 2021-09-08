cb80d748dfaf98dca8c0e0de6fbdbbd93d944d31
walter Van <weijianv@student.unimelb.edu.au>
2021-09-08 17:08:58 +1000

    initial commit with login form

--- web/src/views/Login.tsx
+++ web/src/views/Login.tsx
@@ -1,8 +1,10 @@
 import {
   PrimaryButton, Stack, TextField,
 } from '@fluentui/react';
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { useApp } from '../AppContext';
 
 export interface ILoginProps {
   registering?: boolean;
@@ -10,7 +12,11 @@ export interface ILoginProps {
 
 const stackStyles = {
   root: {
-    backgroundColor: 'lightBlue',
+    backgroundColor: 'white',
+    margin: '10% 0',
+    width: '40%',
+    textAlign: 'center',
+    height: 'fit-content',
   },
 };
 
@@ -19,14 +25,18 @@ const roundStyle = {
 };
 
 const stackTokens = {
-  padding: '10%',
-  childrenGap: '5px',
+  padding: '5% 10%',
+  childrenGap: '10px',
 };
 
 const buttonStyle = {
   root: {
     background: '#5A798D',
     borderRadius: '10px',
+    height: '5em',
+  },
+  label: {
+    fontSize: '1.5em',
   },
 };
 
@@ -35,18 +45,80 @@ const fieldStyles = {
     borderRadius: '10px',
     border: '3px solid #5A798D',
     boxSizing: 'border-box',
-    height: '5em',
+    height: '3em',
   },
-
+  field: {
+    fontSize: '1.5em',
+  },
 };
 
-export const Login: React.VoidFunctionComponent<ILoginProps> = ({ registering }) => (
-  <Stack styles={stackStyles} tokens={stackTokens} style={roundStyle}>
-    <TextField placeholder='Username' styles={fieldStyles} />
-    <TextField type='password' placeholder='Password' styles={fieldStyles} />
-    <PrimaryButton styles={buttonStyle}>{registering ? 'register' : 'login'}</PrimaryButton>
-  </Stack>
-);
+const bodyStyle = {
+  display: 'flex',
+  justifyContent: 'space-around',
+  // background: "#F8F8F8",
+  background: 'red',
+  height: '100%',
+};
+
+export const Login: React.VoidFunctionComponent<ILoginProps> = ({ registering }) => {
+  // get context
+  const [username, setUsername] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const app = useApp();
+  const loginEvent = () => {
+    if (username && password) {
+      app.login(username, password, registering).then((res) => {
+        if (res.ok) {
+          setUsername('');
+          setPassword('');
+        }
+      });
+    }
+  };
+
+  // rudimentary controlled input event catchers
+  const emptyField = (fieldValue: string, fieldName: string) => ((fieldValue) ? '' : `${fieldName} is required`);
+
+  return (
+    <div style={bodyStyle} id='container'>
+      <Stack styles={stackStyles} tokens={stackTokens} style={roundStyle}>
+        <TextField
+          placeholder='Username'
+          styles={fieldStyles}
+          value={username}
+          onGetErrorMessage={(value) => emptyField(value, 'Username')}
+          validateOnFocusOut
+          validateOnLoad={false}
+          onChange={(event) => setUsername(event.currentTarget.value)}
+        />
+        <TextField
+          type='password'
+          placeholder='Password'
+          styles={fieldStyles}
+          value={password}
+          onGetErrorMessage={(value) => emptyField(value, 'Password')}
+          validateOnFocusOut
+          validateOnLoad={false}
+          onChange={(event) => setPassword(event.currentTarget.value)}
+        />
+        <PrimaryButton
+          styles={buttonStyle}
+          onClick={loginEvent}
+          text={registering ? 'Register' : 'Log in'}
+        />
+        <Link to={registering ? '/login' : '/register'}>
+          {
+          registering
+            ? 'Already with an account? Sign in here.'
+            : 'Register to get started'
+        }
+        </Link>
+        <span>Can&apos;t log in?</span>
+      </Stack>
+    </div>
+  );
+};
 
 Login.propTypes = {
   registering: PropTypes.bool,
