d544e0e955d0f57c579ab5acfaa0b5fe2eb9f1b2
oldbugo <j763650979@gmail.com>
2021-09-08 21:23:47 +1000

    added in basic card grid view logic

--- web/src/components/Card.tsx
+++ web/src/components/Card.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { ICard } from '../controllers/Card';
 
 export interface ICardProps {
   card: ICard;
 }
 
-export const Card: React.VoidFunctionComponent<ICardProps> = () => <></>;
+export const Card: React.VoidFunctionComponent<ICardProps> = ({ card }) => {
+  const { name, phone } = card;
+
+  return (
+    <div>
+      <div>{name}</div>
+      <div>{phone}</div>
+    </div>
+  );
+};
+
+Card.propTypes = {
+  card: PropTypes.object as React.Validator<ICard> | undefined,
+};
+
+Card.defaultProps = {
+  card: undefined,
+};
--- web/src/views/Home.tsx
+++ web/src/views/Home.tsx
@@ -1,6 +1,9 @@
 import PropTypes from 'prop-types';
 import React, { createContext, useContext } from 'react';
 import { ICard } from '../controllers/Card';
+import { Card } from '../components/Card';
+
+import { useApp } from '../AppContext';
 
 export interface IHomeContext {
   expandCardDetail(value: boolean): void;
@@ -22,7 +25,25 @@ export interface IHomeProps {
 export const Home: React.VoidFunctionComponent<IHomeProps> = ({ detail }) => {
   function expandCardDetail(): void { }
 
-  return <HomeProvider value={{ expandCardDetail }} />;
+  const { user } = useApp();
+  if (user == null) throw new Error();
+
+  const { cards } = user;
+
+  return (
+    <HomeProvider value={{ expandCardDetail }}>
+
+      <h1>home page</h1>
+      <div>
+        <p>card grid section</p>
+        {cards.map((card) => <Card key={card.id} card={card} />)}
+      </div>
+      <div>
+        <p>card detail section</p>
+      </div>
+
+    </HomeProvider>
+  );
 };
 
 Home.propTypes = {
