e6461696a00dc87a3ed1f232b36db85fea5f3c27
chomosuke <a13323600@gmail.com>
2021-09-10 01:24:45 +1000

    card note field

--- web/src/components/CardDetails/CardNoteField.tsx
+++ web/src/components/CardDetails/CardNoteField.tsx
@@ -1,3 +1,5 @@
+import { Stack, Text, TextField } from '@fluentui/react';
+import { Requireable, bool, object } from 'prop-types';
 import React from 'react';
 import { ICardField } from '../../controllers/CardField';
 
@@ -6,4 +8,36 @@ export interface ICardNoteFieldProps {
   editing: boolean;
 }
 
-export const CardNoteField: React.VoidFunctionComponent<ICardNoteFieldProps> = () => <></>;
+export const CardNoteField: React.VoidFunctionComponent<ICardNoteFieldProps> = (
+  { field, editing },
+) => (
+  <Stack>
+    <Stack.Item align='stretch'>
+      <Text>{field.key}</Text>
+    </Stack.Item>
+    <Stack.Item align='stretch'>
+      {editing
+        ? (
+          <TextField
+            multiline
+            rows={3}
+            resizable={false}
+            value={field.value}
+            onChange={(e, nValue) => {
+              field.update({ value: nValue });
+            }}
+          />
+        )
+        : (
+          <div style={{ borderStyle: 'solid' }}>
+            <Text>{field.value}</Text>
+          </div>
+        )}
+    </Stack.Item>
+  </Stack>
+);
+
+CardNoteField.propTypes = {
+  field: (object as Requireable<ICardField>).isRequired,
+  editing: bool.isRequired,
+};
