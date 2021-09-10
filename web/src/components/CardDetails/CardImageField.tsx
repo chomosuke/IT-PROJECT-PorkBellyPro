bc524d2fd100f1817067ab32fc033ff400a66b57
chomosuke <a13323600@gmail.com>
2021-09-10 17:02:48 +1000

    CardImageField not building

--- web/src/components/CardDetails/CardImageField.tsx
+++ web/src/components/CardDetails/CardImageField.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import Jimp from 'jimp';
+import { Stack } from '@fluentui/react';
+import { Requireable, bool, object } from 'prop-types';
 import { ICard } from '../../controllers/Card';
 
 export interface ICardImageFieldProps {
@@ -6,4 +9,45 @@ export interface ICardImageFieldProps {
   editing: boolean;
 }
 
-export const CardImageField: React.VoidFunctionComponent<ICardImageFieldProps> = () => <></>;
+const [imgWidth, imgHeight] = [500, 250];
+
+export const CardImageField: React.VoidFunctionComponent<ICardImageFieldProps> = (
+  { card, editing },
+) => {
+  const { image } = card;
+  return (
+    <>
+      {image
+    && (editing
+      ? (
+        <Stack>
+          <img src={image} alt='business card' />
+          <input
+            type='file'
+            name='myImage'
+            onChange={async (e) => {
+              if (e.target.files && e.target.files[0]) {
+                const img = e.target.files[0];
+                const jimg = await Jimp.read(Buffer.from(await img.arrayBuffer()));
+                if (jimg.getWidth() / jimg.getHeight() > imgWidth / imgHeight) {
+                  jimg.resize(250, Jimp.AUTO);
+                } else {
+                  jimg.resize(Jimp.AUTO, 500);
+                }
+                const newImageUrl = await jimg.getBase64Async(Jimp.MIME_JPEG);
+                card.update({ image: newImageUrl });
+              }
+            }}
+          />
+        </Stack>
+      )
+      : <img src={image} alt='business card' />
+    )}
+    </>
+  );
+};
+
+CardImageField.propTypes = {
+  card: (object as Requireable<ICard>).isRequired,
+  editing: bool.isRequired,
+};
