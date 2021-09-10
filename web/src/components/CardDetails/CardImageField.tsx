3e207ee553a3be05531847783bd99d8da4353e5e
chomosuke <a13323600@gmail.com>
2021-09-10 19:55:51 +1000

    cardImageField

--- web/src/components/CardDetails/CardImageField.tsx
+++ web/src/components/CardDetails/CardImageField.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Jimp from 'jimp/browser/lib/jimp';
-import { Stack } from '@fluentui/react';
+import { DefaultButton, Stack } from '@fluentui/react';
 import { Requireable, bool, object } from 'prop-types';
 import { ICard } from '../../controllers/Card';
 
@@ -15,12 +15,34 @@ export const CardImageField: React.VoidFunctionComponent<ICardImageFieldProps> =
   { card, editing },
 ) => {
   const { image } = card;
+  const imageComp = image
+    ? (
+      <>
+        <img
+          src={
+            image.substr(0, 4) === 'data'
+              ? image
+              : `${image}?random=${Math.random()}`
+          }
+          alt='business card'
+        />
+        {editing && (
+        <DefaultButton
+          text='delete'
+          onClick={() => {
+            card.update({ image: null });
+          }}
+        />
+        )}
+      </>
+    )
+    : <div style={{ backgroundColor: 'gray', textAlign: 'center' }}>{ card.name }</div>;
   return (
-    <>
+    <Stack>
       {editing
         ? (
-          <Stack>
-            {image && <img src={image} alt='business card' />}
+          <>
+            {imageComp}
             <input
               type='file'
               name='myImage'
@@ -39,10 +61,10 @@ export const CardImageField: React.VoidFunctionComponent<ICardImageFieldProps> =
                 }
               }}
             />
-          </Stack>
+          </>
         )
-        : (image && <img src={image} alt='business card' />)}
-    </>
+        : imageComp}
+    </Stack>
   );
 };
 
