2cceecd862e79c0d5bf950544f2c02fe836354cd
oldbugo <j763650979@gmail.com>
2021-09-10 19:28:58 +1000

    added card detail expansion logic, and card filter logic

--- web/src/components/Card.tsx
+++ web/src/components/Card.tsx
@@ -1,18 +1,57 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { DefaultButton, mergeStyleSets } from '@fluentui/react';
 import { ICard } from '../controllers/Card';
+import { useApp } from '../AppContext';
 
 export interface ICardProps {
   card: ICard;
 }
 
+// styles
+const getClassName = () => mergeStyleSets({
+  root: {
+    margin: '3rem',
+    color: 'white',
+    background: 'limegreen',
+  },
+});
+
 export const Card: React.VoidFunctionComponent<ICardProps> = ({ card }) => {
-  const { name, phone } = card;
+  const {
+    id, name, phone, jobTitle,
+  } = card;
+  const { root } = getClassName();
+  const { showCardDetail } = useApp();
+
+  // const { currentCard, expandCardDetail } = useHome()!;
+
+  /*
+   * const handleOnClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+   *   e.preventDefault;
+   *   const cardId = id;
+   *   expandCardDetail(cardId!);
+   *   console.log(cardId);
+   * }
+   */
+
+  const handleOnClick = () => {
+    console.log('button clicked');
+    showCardDetail(card);
+  };
 
   return (
-    <div>
+    <div className={root}>
+
+      <div>{id}</div>
       <div>{name}</div>
       <div>{phone}</div>
+      <div>{jobTitle}</div>
+
+      <DefaultButton onClick={handleOnClick}>
+        select this
+      </DefaultButton>
+
     </div>
   );
 };
--- web/src/views/Home.tsx
+++ web/src/views/Home.tsx
@@ -1,35 +1,97 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useState } from 'react';
+import { Stack } from '@fluentui/react';
 import { useApp } from '../AppContext';
 import { HomeProvider } from '../HomeContext';
 import { Card } from '../components/Card';
 import { ICard } from '../controllers/Card';
+import { CardDetails } from '../components/CardDetails';
 
 export interface IHomeProps {
   detail?: ICard;
 }
 
+/// ///// fluent-ui stack styling ////////
+
+/*
+ *const stackStyles: IStackStyles = {
+ *root: {
+ *  background: DefaultPalette.themePrimary,
+ *},
+ *};
+ */
+
+/*
+ * const StackItemStyles: IStackItemStyles = {
+ *   root: {
+ *     alignItems: 'center',
+ *     display: 'flex',
+ *     height: '50vh',
+ *     justifyContent: 'center',
+ *   },
+ * };
+ */
+
+/*
+ * const stackTokens: IStackTokens = {
+ *   childrenGap: 5,
+ *   padding: 20,
+ * };
+ */
+
 /* eslint-disable-next-line @typescript-eslint/no-unused-vars */
 export const Home: React.VoidFunctionComponent<IHomeProps> = ({ detail }) => {
-  function expandCardDetail(): void { }
+  // const [ currentCard, expandCardDetail ] = useState<string | undefined>(defaultStatus);
 
-  const { user } = useApp();
+  const [expand, setExpand] = useState(false);
+  const { user, searchQuery } = useApp();
   if (user == null) throw new Error();
 
   const { cards } = user;
 
+  function expandCardDetail(value: boolean): void {
+    setExpand(value);
+  }
+
+  function filterCard(card: ICard): boolean {
+    const searchedTokens = searchQuery.split(/\W/).filter((x) => x.length > 0);
+
+    const searchedCard = [
+      ...card.name.split(/\W/).filter((x) => x.length > 0),
+      ...card.phone.split(/\W/).filter((x) => x.length > 0),
+      ...card.email.split(/\W/).filter((x) => x.length > 0),
+      ...card.jobTitle.split(/\W/).filter((x) => x.length > 0),
+      ...card.company.split(/\W/).filter((x) => x.length > 0),
+      ...card.fields.map((field) => field.value.split(/\W/).filter((x) => x.length > 0))
+        .reduce((a, b) => a.concat(b), []),
+    ];
+
+    return searchedTokens.every((sToken) => searchedCard.some(
+      (cToken) => cToken.includes(sToken),
+    ));
+  }
+
   return (
     <HomeProvider value={{ expandCardDetail }}>
 
-      <h1>home page</h1>
-      <div>
-        <p>card grid section</p>
-        {cards.map((card) => <Card key={card.id} card={card} />)}
-      </div>
-      <div>
-        <p>card detail section</p>
-      </div>
+      <Stack horizontal>
 
+        <Stack.Item grow={2}>
+          <Stack horizontal wrap>
+            {cards.filter(filterCard).map((card) => <Card key={card.id} card={card} />)}
+          </Stack>
+        </Stack.Item>
+
+        {detail != null
+          && (
+          <Stack.Item grow={expand ? 2 : 1}>
+
+            <CardDetails card={detail} editing />
+
+          </Stack.Item>
+          )}
+
+      </Stack>
     </HomeProvider>
   );
 };
