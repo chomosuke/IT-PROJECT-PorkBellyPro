5ac301e71893617ea9f97058f2ef8a3017c5319f
chomosuke <a13323600@gmail.com>
2021-09-16 00:37:18 +1000

    replaced hasImage with imageHash in shared workspace type defs

--- shared/src/api-types/CardPatchRequest.ts
+++ shared/src/api-types/CardPatchRequest.ts
@@ -1,6 +1,6 @@
 import type { Card } from './Card';
 import type { Image } from './Image';
 
-export interface CardPatchRequest extends Pick<Card, 'id'>, Partial<Omit<Card, 'id' | 'hasImage'>> {
+export interface CardPatchRequest extends Pick<Card, 'id'>, Partial<Omit<Card, 'id' | 'imageHash'>> {
   image?: Image | null;
 }
