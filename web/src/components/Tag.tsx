29ba5bc93239b499f3f8400885e4306f758ee7b6
pep1032314 <pphowarasoon@student.unimelb.edu.au>
2021-09-17 13:09:07 +0700

    modify Tag.tsx and TagButton.tsx

--- web/src/components/Tag.tsx
+++ web/src/components/Tag.tsx
@@ -1,4 +1,6 @@
-import { IButtonProps } from '@fluentui/react';
+import {
+  DefaultButton, IButtonProps, IIconProps, IconButton, Stack,
+} from '@fluentui/react';
 import PropTypes, { Requireable } from 'prop-types';
 import React from 'react';
 import { ITag } from '../controllers/Tag';
@@ -11,7 +13,69 @@ export interface ITagProps {
   onRemove?: OnClickHandler;
 }
 
-export const Tag: React.VoidFunctionComponent<ITagProps> = () => null;
+const removeIcon: IIconProps = { iconName: 'CalculatorMultiply' };
+
+export const Tag: React.VoidFunctionComponent<ITagProps> = ({ tag, onClick, onRemove }) => {
+  const tagWithXStyles = {
+    root: {
+      height: '20px',
+      outerWidth: '50px',
+      backgroundColor: tag.color,
+      border: 'none',
+    },
+  };
+
+  const removeBtnStyles = {
+    root: {
+      height: '20px',
+      outerWidth: '10px',
+      fontSize: '5px',
+      backgroundColor: tag.color,
+      border: 'none',
+    },
+  };
+
+  const tagStyles = {
+    root: {
+      height: '20px',
+      outerWidth: '60px',
+      backgroundColor: tag.color,
+      border: 'none',
+    },
+  };
+
+  if ({ onRemove } !== undefined) {
+    // show x button if on remove is pass in
+    return (
+      <Stack horizontal>
+        <DefaultButton
+          text={tag.label}
+          onClick={onClick}
+          styles={tagWithXStyles}
+        />
+        <IconButton
+          iconProps={removeIcon}
+          onClick={onRemove}
+          styles={removeBtnStyles}
+        />
+      </Stack>
+    );
+  }
+
+  // otherwise only show the tag itself
+  return (
+    <DefaultButton
+      text={tag.label}
+      onClick={onClick}
+      styles={tagStyles}
+    />
+  );
+};
+
+Tag.defaultProps = {
+  onClick: undefined,
+  onRemove: undefined,
+};
 
 Tag.propTypes = {
   tag: (PropTypes.object as Requireable<ITag>).isRequired,
--- web/src/components/TagButton.tsx
+++ web/src/components/TagButton.tsx
@@ -1,4 +1,4 @@
-import { IButtonProps } from '@fluentui/react';
+import { DefaultButton, IButtonProps } from '@fluentui/react';
 import PropTypes, { Requireable } from 'prop-types';
 import React from 'react';
 import { ITag } from '../controllers/Tag';
@@ -10,7 +10,28 @@ export interface ITagButtonProps {
   onClick?: OnClickHandler;
 }
 
-export const TagButton: React.VoidFunctionComponent<ITagButtonProps> = () => null;
+export const TagButton: React.VoidFunctionComponent<ITagButtonProps> = ({ tag, onClick }) => {
+  const tagStyles = {
+    root: {
+      height: '40px',
+      outerWidth: '60px',
+      backgroundColor: tag.color,
+      border: 'none',
+    },
+  };
+
+  return (
+    <DefaultButton
+      text={tag.label}
+      onClick={onClick}
+      styles={tagStyles}
+    />
+  );
+};
+
+TagButton.defaultProps = {
+  onClick: undefined,
+};
 
 TagButton.propTypes = {
   tag: (PropTypes.object as Requireable<ITag>).isRequired,
