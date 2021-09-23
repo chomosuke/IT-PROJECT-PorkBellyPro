e87a4ff23247e22ed48289803401f2e97e7c2058
Shang Zhe Lee <shangzhel@student.unimelb.edu.au>
2021-09-23 12:43:16 +1000

    Remove eslint-disable comments.

--- web/src/components/cardDetails/processImage.ts
+++ web/src/components/cardDetails/processImage.ts
@@ -1,12 +1,13 @@
-import Jimp from 'jimp/browser/lib/jimp';
-// eslint-disable-next-line no-restricted-globals
-self.onmessage = async ({ data: { img, imgWidth, imgHeight } }) => {
-  const jimg = await Jimp.read(Buffer.from(await img.arrayBuffer()));
+import { AUTO, MIME_JPEG, read } from 'jimp/browser/lib/jimp';
+
+onmessage = async ({ data: { img, imgWidth, imgHeight } }) => {
+  // Buffer polyfill comes from jimp/browser/lib/jimp
+  const jimg = await read(Buffer.from(await img.arrayBuffer()));
   if (jimg.getWidth() / jimg.getHeight() > imgWidth / imgHeight) {
-    jimg.resize(imgWidth, Jimp.AUTO);
+    jimg.resize(imgWidth, AUTO);
   } else {
-    jimg.resize(Jimp.AUTO, imgHeight);
+    jimg.resize(AUTO, imgHeight);
   }
-  // eslint-disable-next-line no-restricted-globals
-  self.postMessage({ url: await jimg.getBase64Async(Jimp.MIME_JPEG) });
+
+  postMessage({ url: await jimg.getBase64Async(MIME_JPEG) });
 };
