2a82ddc9e1d9263ac74d04dc4c6e99489366d7e0
walter Van <weijianv@student.unimelb.edu.au>
2021-09-23 14:16:02 +1000

    linked addTag context callback. Preferred array of refs over useId See https://github.com/chomosuke/IT-PROJECT-PorkBellyPro/issues/84#issuecomment-925492480

--- web/src/components/TagPicker.tsx
+++ web/src/components/TagPicker.tsx
@@ -5,10 +5,12 @@ import PropTypes from 'prop-types';
 import {
   Callout, DefaultButton, Stack, TextField,
 } from '@fluentui/react';
+import { useId } from '@fluentui/react-hooks';
 import { ICard } from '../controllers/Card';
 import { useApp } from '../AppContext';
 import { Tag } from './Tag';
 import { ITagEditorProps, TagEditor } from './TagEditor';
+import { ITagProperties } from '../controllers/Tag';
 
 export interface ITagPickerProps {
   targetCard: ICard;
@@ -19,7 +21,16 @@ export const TagPicker: React.VoidFunctionComponent<ITagPickerProps> = ({ target
   const [pickerActive, setPickerActive] = useState<boolean>(false);
   const [tagSearchString, setTagSearchString] = useState<string>('');
   const [focusedTag, setFocusedTag] = useState<ITagEditorProps>();
-  const calloutTarget = useRef(null);
+  const pickerTargetId = useId('picker-target');
+  // map stores tag id keys and element values
+  const tagTargetRefs = useRef(new Map());
+
+  function getNewTag(): void {
+    const tagProps: Partial<ITagProperties> = {
+      label: tagSearchString || 'New Tag',
+    };
+    user.newTag(tagProps);
+  }
 
   // user variable is used to access the tags available to the user
   return (
@@ -27,20 +38,18 @@ export const TagPicker: React.VoidFunctionComponent<ITagPickerProps> = ({ target
       <Stack.Item grow key='field'>
         Tags
       </Stack.Item>
-      <Stack.Item grow key='tags'>
+      <Stack.Item grow key='tags' id={pickerTargetId}>
         {/* could style to fit measurements */}
         <Stack horizontal>
           {targetCard?.tags.map((t) => <Tag tag={t} onRemove={() => { }} />)}
         </Stack>
 
       </Stack.Item>
-      <div ref={calloutTarget}>
-        <DefaultButton text='AttachCard' onClick={() => setPickerActive((old) => !old)} />
-      </div>
+      <DefaultButton text='AttachCard' onClick={() => setPickerActive((old) => !old)} />
       {pickerActive
         ? (
           <Callout
-            target={calloutTarget.current}
+            target={`#${pickerTargetId}`}
             onDismiss={() => setPickerActive(false)}
           >
             <Stack>
@@ -50,32 +59,34 @@ export const TagPicker: React.VoidFunctionComponent<ITagPickerProps> = ({ target
                   value={tagSearchString}
                   onChange={(event) => setTagSearchString(event.currentTarget.value)}
                 />
-                <DefaultButton text='CreateTag' />
+                <DefaultButton text='CreateTag' onClick={getNewTag} />
               </Stack.Item>
               {/* Tags available to use are listed here */}
               {user.user?.tags
                 .filter((t) => t.label.toUpperCase().startsWith(tagSearchString.toUpperCase()))
                 .filter((t) => !targetCard?.tags.includes(t))
                 .map((t) => (
-                  <Stack.Item key={t.id}>
-                    <Tag tag={t} />
-                    <DefaultButton
-                      text='EditTag'
-                      onClick={(event) => setFocusedTag({
-                        tag: t,
-                        anchor: event.currentTarget as Element,
-                      })}
-                    />
-                  </Stack.Item>
+                  <div ref={(el) => tagTargetRefs.current.set(t.id, el)}>
+                    <Stack.Item key={t.id}>
+                      <Tag tag={t} />
+                      <DefaultButton
+                        text='EditTag'
+                        onClick={() => setFocusedTag({
+                          tag: t,
+                          anchor: tagTargetRefs.current.get(t.id),
+                        })}
+                      />
+                    </Stack.Item>
+                  </div>
                 ))}
             </Stack>
             {focusedTag
               && (
-              <TagEditor
-                tag={focusedTag.tag}
-                anchor={focusedTag.anchor}
-                closingFunction={() => setFocusedTag(undefined)}
-              />
+                <TagEditor
+                  tag={focusedTag.tag}
+                  anchor={focusedTag.anchor}
+                  closingFunction={() => setFocusedTag(undefined)}
+                />
               )}
           </Callout>
         )
