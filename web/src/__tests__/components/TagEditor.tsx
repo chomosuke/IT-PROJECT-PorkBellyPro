837169344c3df59f690d338323298868fe99fcde
walter Van <weijianv@student.unimelb.edu.au>
2021-09-24 18:19:23 +1000

    written render tests for TagPicker, TagWrapper, and TagEditor

---
+++ web/src/__tests__/components/TagEditor.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { create } from 'react-test-renderer';
+import { TagEditor } from '../../components/TagEditor';
+import { ITag } from '../../controllers/Tag';
+
+import '../disable-icon-warnings.helpers';
+
+const demoTag: ITag = {
+  id: '1',
+  label: 'Big Boss',
+  color: '#BF7829',
+  commit() { throw new Error('Not implemented'); },
+  delete() { throw new Error('Not implemented'); },
+};
+
+const htmlId = 'TagAnchor';
+
+describe('TagEditor render tests', () => {
+  test('without closingFunction', () => {
+    const json = create(
+      <TagEditor anchor={htmlId} tag={demoTag} />,
+    ).toJSON();
+    expect(json).toMatchInlineSnapshot(`
+<span
+  className="ms-layer"
+/>
+`);
+  });
+
+  test('with closingFunction', () => {
+    const json = create(
+      <TagEditor
+        anchor={htmlId}
+        tag={demoTag}
+        closingFunction={() => { }}
+      />,
+    ).toJSON();
+    expect(json).toMatchInlineSnapshot(`
+<span
+  className="ms-layer"
+/>
+`);
+  });
+});
