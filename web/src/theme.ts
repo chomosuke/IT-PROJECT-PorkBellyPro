d1907bf32af4a527642ecc8fda9547ea7896a00e
Shang Zhe Lee <shangzhel@student.unimelb.edu.au>
2021-09-27 20:38:13 +1000

    Add theme fonts.

--- web/src/theme.ts
+++ web/src/theme.ts
@@ -16,7 +16,28 @@ export interface Shape {
   default: IRawStyle;
 }
 
+export interface FontFamily {
+  roboto: IRawStyle;
+  ubuntu: IRawStyle;
+}
+
+export interface FontWeight {
+  light: IRawStyle;
+  medium: IRawStyle;
+  bold: IRawStyle;
+  black: IRawStyle;
+}
+
+export interface FontSize {
+  size16: IRawStyle;
+  size24: IRawStyle;
+  size36: IRawStyle;
+}
+
 export interface Theme {
   palette: Palette;
   shape: Shape;
+  fontFamily: FontFamily;
+  fontWeight: FontWeight;
+  fontSize: FontSize;
 }
