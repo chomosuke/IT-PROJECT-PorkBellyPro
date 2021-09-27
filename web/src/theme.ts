1b54ed440a39e5eaa7a62b7f9df04859579aff45
Shang Zhe Lee <shangzhel@student.unimelb.edu.au>
2021-09-27 20:21:06 +1000

    Add theme palette.

---
+++ web/src/theme.ts
@@ -0,0 +1,15 @@
+export interface Palette {
+  darkDenim: string;
+  everblue: string;
+  stoneBlue: string;
+  moldyCheese: string;
+  justWhite: string;
+  quartz: string;
+  cloudyDay: string;
+  deepSlate: string;
+  sootyBee: string;
+}
+
+export interface Theme {
+  palette: Palette;
+}
