8e38a3dbc0b41c5c440906d694c286d53c09a238
Shang Zhe Lee <shangzhel@student.unimelb.edu.au>
2021-09-27 22:25:56 +1000

    Add theme defaults.

--- web/src/theme.ts
+++ web/src/theme.ts
@@ -1,5 +1,23 @@
 import { IRawStyle } from '@fluentui/react';
-import { Icon as PhosphorIcon } from 'phosphor-react';
+import {
+  CaretDoubleLeft,
+  CaretDoubleRight,
+  CaretDown,
+  CaretLeft,
+  CaretRight,
+  CaretUp,
+  FloppyDisk,
+  IconProps,
+  MagnifyingGlass,
+  MinusCircle,
+  PencilLine,
+  Icon as PhosphorIcon,
+  PlusCircle,
+  SignOut,
+  Trash,
+  UserCircle,
+} from 'phosphor-react';
+import { createElement, forwardRef } from 'react';
 
 export interface Palette {
   darkDenim: string;
@@ -62,3 +80,60 @@ export interface Theme {
   fontSize: FontSize;
   icon: Icon;
 }
+
+function makeIcon(icon: PhosphorIcon, weight: NonNullable<IconProps['weight']>): PhosphorIcon {
+  return forwardRef<SVGSVGElement>((props, ref) => createElement(icon, { ...props, weight, ref }));
+}
+
+export const defaultTheme: Theme = {
+  palette: {
+    darkDenim: '#1a3240',
+    everblue: '#2f5972',
+    stoneBlue: '#627b8a',
+    moldyCheese: '#8c99a1',
+    justWhite: '#ffffff',
+    quartz: '#f8f8f8',
+    cloudyDay: '#c7c7c7',
+    deepSlate: '#5e5e5e',
+    sootyBee: '#0f0d0d',
+  },
+  shape: {
+    default: {
+      dropShadow: '2px 4px 4px rgb(0,0,0,0.25)',
+      borderRadius: '8px',
+    },
+  },
+  fontFamily: {
+    roboto: { fontFamily: 'Roboto' },
+    ubuntu: { fontFamily: 'Ubuntu' },
+  },
+  fontWeight: {
+    light: { fontWeight: '100' },
+    medium: { fontWeight: '400' },
+    bold: { fontWeight: '700' },
+    black: { fontWeight: '900' },
+  },
+  fontSize: {
+    size16: { fontSize: '16px' },
+    size24: { fontSize: '24px' },
+    size36: { fontSize: '36px' },
+  },
+  icon: {
+    plusCircleBold: makeIcon(PlusCircle, 'bold'),
+    minusCircleBold: makeIcon(MinusCircle, 'bold'),
+    userCircleBold: makeIcon(UserCircle, 'bold'),
+    caretDownBold: makeIcon(CaretDown, 'bold'),
+    caretUpBold: makeIcon(CaretUp, 'bold'),
+    caretLeftBold: makeIcon(CaretLeft, 'bold'),
+    caretRightBold: makeIcon(CaretRight, 'bold'),
+    caretDoubleLeftBold: makeIcon(CaretDoubleLeft, 'bold'),
+    caretDoubleRightBold: makeIcon(CaretDoubleRight, 'bold'),
+    pencilLineBold: makeIcon(PencilLine, 'bold'),
+    trashBold: makeIcon(Trash, 'bold'),
+    floppyDiskBold: makeIcon(FloppyDisk, 'bold'),
+    magnifyingGlassLight: makeIcon(MagnifyingGlass, 'light'),
+    caretDownLight: makeIcon(CaretDown, 'light'),
+    signOutLight: makeIcon(SignOut, 'light'),
+    plusCircleLight: makeIcon(PlusCircle, 'light'),
+  },
+};
