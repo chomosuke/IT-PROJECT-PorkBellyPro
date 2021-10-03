25168230d83199084ab4e52c5a50b0039868aa22
pep1032314 <pphowarasoon@student.unimelb.edu.au>
2021-10-03 13:55:36 +0700

    Apply styles to dialogs, delete LoginView test

--- web/src/components/dialogs/DeletingTagWarning.tsx
+++ web/src/components/dialogs/DeletingTagWarning.tsx
@@ -1,8 +1,10 @@
 import {
-  DefaultButton, Dialog, DialogFooter, DialogType, IButtonProps, PrimaryButton,
+  DefaultButton, Dialog, DialogFooter, DialogType, IButtonProps,
+  IButtonStyles, PrimaryButton, Stack,
 } from '@fluentui/react';
 import PropTypes from 'prop-types';
 import React from 'react';
+import { useTheme } from '../../theme';
 
 type OnClickHandler = IButtonProps['onClick'];
 
@@ -12,27 +14,102 @@ export interface IDialogProps {
   onDelete?: OnClickHandler;
 }
 
-const tagContentProps = {
-  type: DialogType.normal,
-  title: 'Warning',
-  closeButtonAriaLabel: 'Close',
-  subText: 'Deleted tags won\'t be recoverable, are you sure you want to do that?',
-};
-
 export const DeletingTagWarning: React.VoidFunctionComponent<IDialogProps> = (
   { hideDialog, toggleHideDialog, onDelete },
-) => (
-  <Dialog
-    hidden={hideDialog}
-    onDismiss={toggleHideDialog}
-    dialogContentProps={tagContentProps}
-  >
-    <DialogFooter>
-      <PrimaryButton onClick={onDelete} text='Yes, Delete' />
-      <DefaultButton onClick={toggleHideDialog} text='Close' />
-    </DialogFooter>
-  </Dialog>
-);
+) => {
+  const theme = useTheme();
+
+  const yesBtnStyles:IButtonStyles = {
+    root: {
+      ...theme.fontFamily.roboto,
+      ...theme.fontSize.small,
+      ...theme.fontWeight.bold,
+      backgroundColor: theme.palette.justWhite,
+      color: theme.palette.everblue,
+      borderStyle: 'solid',
+      borderWidth: '2px',
+      borderColor: theme.palette.justWhite,
+      whiteSpace: 'nowrap',
+      padding: '0 24px',
+      margin: '0 12px',
+      height: '40px',
+      borderRadius: '4px',
+    },
+  };
+
+  const closeBtnStyles:IButtonStyles = {
+    root: {
+      ...theme.fontFamily.roboto,
+      ...theme.fontSize.small,
+      ...theme.fontWeight.bold,
+      backgroundColor: theme.palette.everblue,
+      color: theme.palette.justWhite,
+      borderStyle: 'solid',
+      borderWidth: '2px',
+      borderColor: theme.palette.justWhite,
+      whiteSpace: 'nowrap',
+      padding: '0 24px',
+      margin: '0 12px',
+      height: '40px',
+      borderRadius: '4px',
+    },
+  };
+
+  const dialogStyles = {
+    main: {
+      width: '340px',
+      backgroundColor: theme.palette.everblue,
+      borderRadius: '4px',
+    },
+
+  };
+
+  const dialogContentStyle = {
+    title: {
+      ...theme.fontFamily.roboto,
+      ...theme.fontSize.standard,
+      ...theme.fontWeight.bold,
+      color: theme.palette.justWhite,
+      padding: '24px',
+    },
+    subText: {
+      ...theme.fontFamily.roboto,
+      ...theme.fontSize.small,
+      ...theme.fontWeight.light,
+      color: theme.palette.justWhite,
+    },
+    inner: {
+      display: 'flex',
+      justifyItems: 'space-between',
+      flexDirection: 'column',
+      padding: '0px 24px 24px 24px',
+    },
+  };
+
+  const tagContentProps = {
+    type: DialogType.normal,
+    title: 'Warning',
+    closeButtonAriaLabel: 'Close',
+    subText: 'Deleted tags won\'t be recoverable, are you sure you want to do that?',
+    styles: dialogContentStyle,
+  };
+
+  return (
+    <Dialog
+      hidden={hideDialog}
+      onDismiss={toggleHideDialog}
+      dialogContentProps={tagContentProps}
+      styles={dialogStyles}
+    >
+      <DialogFooter>
+        <Stack horizontal horizontalAlign='end'>
+          <PrimaryButton styles={yesBtnStyles} onClick={onDelete} text='Yes, Delete' />
+          <DefaultButton styles={closeBtnStyles} onClick={toggleHideDialog} text='Cancel' />
+        </Stack>
+      </DialogFooter>
+    </Dialog>
+  );
+};
 
 DeletingTagWarning.defaultProps = {
   onDelete: undefined,
--- web/src/components/dialogs/LoginAndRegisterError.tsx
+++ web/src/components/dialogs/LoginAndRegisterError.tsx
@@ -1,8 +1,9 @@
 import {
-  DefaultButton, Dialog, DialogFooter, DialogType,
+  DefaultButton, Dialog, DialogFooter, DialogType, IButtonStyles,
 } from '@fluentui/react';
 import PropTypes from 'prop-types';
 import React from 'react';
+import { useTheme } from '../../theme';
 
 export interface IDialogProps {
   hideDialog: boolean | undefined;
@@ -10,23 +11,78 @@ export interface IDialogProps {
   registering?: boolean;
 }
 
-const registerationContentProps = {
-  type: DialogType.normal,
-  title: 'Error',
-  closeButtonAriaLabel: 'Close',
-  subText: 'Your Username has already been taken',
-};
-
-const loginContentProps = {
-  type: DialogType.normal,
-  title: 'Error',
-  closeButtonAriaLabel: 'Close',
-  subText: 'Incorrect username or password',
-};
-
 export const LoginAndRegisterError: React.VoidFunctionComponent<IDialogProps> = (
   { hideDialog, toggleHideDialog, registering },
 ) => {
+  const theme = useTheme();
+
+  const closeBtnStyles:IButtonStyles = {
+    root: {
+      ...theme.fontFamily.roboto,
+      ...theme.fontSize.small,
+      ...theme.fontWeight.bold,
+      backgroundColor: theme.palette.justWhite,
+      color: theme.palette.everblue,
+      borderStyle: 'solid',
+      borderWidth: '2px',
+      borderColor: theme.palette.justWhite,
+      whiteSpace: 'nowrap',
+      padding: '0 24px',
+      height: '40px',
+      borderRadius: '4px',
+    },
+  };
+
+  const dialogStyles = {
+    main: {
+      width: '340px',
+      backgroundColor: theme.palette.everblue,
+      borderRadius: '4px',
+    },
+
+  };
+
+  const dialogContentStyle = {
+    title: {
+      ...theme.fontFamily.roboto,
+      ...theme.fontSize.standard,
+      ...theme.fontWeight.bold,
+      color: theme.palette.justWhite,
+      padding: '24px',
+    },
+    subText: {
+      ...theme.fontFamily.roboto,
+      ...theme.fontSize.small,
+      ...theme.fontWeight.light,
+      color: theme.palette.justWhite,
+    },
+    inner: {
+      display: 'flex',
+      justifyItems: 'space-between',
+      flexDirection: 'column',
+      padding: '0px 24px 24px 24px',
+    },
+    innerContent: {
+      paddingBottom: '50px',
+    },
+  };
+
+  const registerationContentProps = {
+    type: DialogType.normal,
+    title: 'Error',
+    closeButtonAriaLabel: 'Close',
+    subText: 'Your Username has already been taken',
+    styles: dialogContentStyle,
+  };
+
+  const loginContentProps = {
+    type: DialogType.normal,
+    title: 'Error',
+    closeButtonAriaLabel: 'Close',
+    subText: 'Incorrect username or password',
+    styles: dialogContentStyle,
+  };
+
   // case 1: registeration failure
   if (registering) {
     return (
@@ -34,9 +90,10 @@ export const LoginAndRegisterError: React.VoidFunctionComponent<IDialogProps> = 
         hidden={hideDialog}
         onDismiss={toggleHideDialog}
         dialogContentProps={registerationContentProps}
+        styles={dialogStyles}
       >
         <DialogFooter>
-          <DefaultButton onClick={toggleHideDialog} text='Close' />
+          <DefaultButton styles={closeBtnStyles} onClick={toggleHideDialog} text='Close' />
         </DialogFooter>
       </Dialog>
     );
@@ -48,9 +105,10 @@ export const LoginAndRegisterError: React.VoidFunctionComponent<IDialogProps> = 
       hidden={hideDialog}
       onDismiss={toggleHideDialog}
       dialogContentProps={loginContentProps}
+      styles={dialogStyles}
     >
       <DialogFooter>
-        <DefaultButton onClick={toggleHideDialog} text='Close' />
+        <DefaultButton styles={closeBtnStyles} onClick={toggleHideDialog} text='Close' />
       </DialogFooter>
     </Dialog>
   );
