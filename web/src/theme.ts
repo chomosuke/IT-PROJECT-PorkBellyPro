3d6a5b0542108b3242a2dd244b75aeac2660024b
oldbugo <j763650979@gmail.com>
2021-10-06 15:37:37 +1100

    Added and changed existing font-size and font-weights. Changed the weight of various icons.

--- web/src/theme.ts
+++ web/src/theme.ts
@@ -48,7 +48,9 @@ export interface FontFamily {
 }
 
 export interface FontWeight {
+  thin: IRawStyle;
   light: IRawStyle;
+  regular: IRawStyle;
   medium: IRawStyle;
   bold: IRawStyle;
   black: IRawStyle;
@@ -78,6 +80,7 @@ export interface Icon {
   signOut: PhosphorIcon;
   plusCircleTag: PhosphorIcon;
   folderOpen: PhosphorIcon;
+  trashBold: PhosphorIcon;
   dotsThree: PhosphorIcon;
 }
 
@@ -144,34 +147,37 @@ export const defaultTheme = freezeDeep<Theme>({
     ubuntu: { fontFamily: 'Ubuntu, sans-serif' },
   },
   fontWeight: {
-    light: { fontWeight: '100' },
-    medium: { fontWeight: '400' },
+    thin: { fontWeight: '100' },
+    light: { fontWeight: '300' },
+    regular: { fontWeight: '400' },
+    medium: { fontWeight: '500' },
     bold: { fontWeight: '700' },
     black: { fontWeight: '900' },
   },
   fontSize: {
     small: { fontSize: '16px' },
-    standard: { fontSize: '24px' },
+    standard: { fontSize: '20px' },
     title: { fontSize: '36px' },
   },
   icon: {
-    plusCircle: makeIcon(PlusCircle, 'bold'),
-    minusCircle: makeIcon(MinusCircle, 'bold'),
-    userCircle: makeIcon(UserCircle, 'bold'),
+    plusCircle: makeIcon(PlusCircle, 'regular'),
+    minusCircle: makeIcon(MinusCircle, 'regular'),
+    userCircle: makeIcon(UserCircle, 'regular'),
     caretDown: makeIcon(CaretDown, 'bold'),
     caretUp: makeIcon(CaretUp, 'bold'),
     caretLeft: makeIcon(CaretLeft, 'bold'),
     caretRight: makeIcon(CaretRight, 'bold'),
-    caretDoubleLeft: makeIcon(CaretDoubleLeft, 'bold'),
-    caretDoubleRight: makeIcon(CaretDoubleRight, 'bold'),
-    pencilLine: makeIcon(PencilLine, 'bold'),
-    trash: makeIcon(Trash, 'bold'),
+    caretDoubleLeft: makeIcon(CaretDoubleLeft, 'regular'),
+    caretDoubleRight: makeIcon(CaretDoubleRight, 'regular'),
+    pencilLine: makeIcon(PencilLine, 'regular'),
+    trash: makeIcon(Trash, 'regular'),
     tick: makeIcon(Check, 'bold'),
     cross: makeIcon(X, 'bold'),
     magnifyingGlass: makeIcon(MagnifyingGlass, 'light'),
     signOut: makeIcon(SignOut, 'light'),
     plusCircleTag: makeIcon(PlusCircle, 'bold'),
     folderOpen: makeIcon(FolderOpen, 'bold'),
+    trashBold: makeIcon(Trash, 'bold'),
     dotsThree: makeIcon(DotsThree, 'bold'),
   },
 });
