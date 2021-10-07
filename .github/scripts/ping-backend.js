e6a4c47d2ac3c9cac087e4da9225416575eaa7e4
walter Van <weijianv@student.unimelb.edu.au>
2021-10-07 20:59:24 +1100

    changed ping script to ping at intervals

--- .github/scripts/ping-backend.js
+++ .github/scripts/ping-backend.js
@@ -1,18 +1,26 @@
 const net = require('net');
 const { exit } = require('process');
 
-const CONN_TIME = 10000;
+const MAX_TRIES = 10;
+const PING_TTL = 1000;
+const port = process.env.SERVER_PORT || 8080;
+let tries = 0;
 
-const connsock = new net.Socket();
-console.error("pinging localhost:%d", process.env.SERVER_PORT);
-connsock.on('error', () => { });
-setTimeout(() => {
-    console.error('Unable to connect to server in %d sec', CONN_TIME / 1000);
-    exit(1);
-}, CONN_TIME);
-
-connsock.connect(process.env.SERVER_PORT, () => {
-    connsock.end();
-    console.log("Connected!")
-    exit(0);
+console.log("Attempting TCP connection to localhost:%d", port);
+let connsock = new net.Socket();
+connsock.on('error', () => {
+    tries++;
+    console.error("No reponse -- tries: %d", tries);
+    if (tries >= MAX_TRIES) {
+        console.error("Unable to reach localhost:%d in %d seconds", port, MAX_TRIES * PING_TTL / 1000);
+        exit(1);
+    }
 })
+// gives ttl for connection
+
+setInterval(() => {
+    connsock.connect(port, () => {
+        console.log("Connected to server");
+        exit(0);
+    });
+}, PING_TTL);
