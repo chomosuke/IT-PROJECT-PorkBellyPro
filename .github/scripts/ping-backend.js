f712e94ef238c98c4719a606c119ce41f51016ae
walter Van <weijianv@student.unimelb.edu.au>
2021-10-07 19:44:04 +1100

    debugging background server spin

--- .github/scripts/ping-backend.js
+++ .github/scripts/ping-backend.js
@@ -4,8 +4,8 @@ const { exit } = require('process');
 const CONN_TIME = 10000;
 
 const connsock = new net.Socket();
+console.error("pinging localhost:%d", process.env.SERVER_PORT);
 connsock.on('error', () => { });
-console.log("pinging localhost:%d", process.env.SERVER_PORT);
 setTimeout(() => {
     console.error('Unable to connect to server in %d sec', CONN_TIME / 1000);
     exit(1);
