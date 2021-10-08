73db75b49c6cd2088cef96d819df07eca310ff59
Shang Zhe Lee <shangzhel@student.unimelb.edu.au>
2021-10-09 01:18:34 +1100

    Lift API route interception.

--- web/src/__tests__/__acceptance__/ac01.ts
+++ web/src/__tests__/__acceptance__/ac01.ts
@@ -2,13 +2,45 @@ import {
   button, click, focus, textBox, write,
 } from 'taiko';
 
-import './common';
+import { gotoHome, interceptRequests } from './common';
 
 test('AC01: Successful registration', async () => {
+  // There is one assertion at the end.
   expect.assertions(1);
 
-  await click('Register to get started');
+  /*
+   * Optional: Intercept fetches to mock server responses.
+   * Skip the call to interceptRequests if your test is intended to run against a real server
+   * instance.
+   */
+  const reRegister = /^\/register$/i;
+  await interceptRequests(async (respond, request) => {
+    const match = reRegister.exec(request.url);
+    // No route match, return false to indicate route is not handled.
+    if (match == null) return false;
+
+    if (request.hasPostData && request.postData != null) {
+      let body;
+      try {
+        body = JSON.parse(request.postData);
+      } catch { } // eslint-disable-line no-empty
+      if (body != null) {
+        const { username, password } = body;
+        if (typeof username === 'string'
+          && typeof password === 'string') {
+          await respond({ status: 201 });
+          return true;
+        }
+      }
+    }
+
+    await respond({ status: 400 });
+    return true;
+  });
+
   // Precondition: User is on the registration page
+  await gotoHome();
+  await click('Register to get started');
 
   // Step: Enter a unique username into the username field
   await focus(textBox('Username'));
--- web/src/__tests__/__acceptance__/common.ts
+++ web/src/__tests__/__acceptance__/common.ts
@@ -2,11 +2,11 @@ import { readFile } from 'fs/promises';
 import { getType } from 'mime';
 import { normalize, resolve } from 'path';
 import {
-  InterceptMockData, InterceptRequest, closeBrowser, goto, intercept, openBrowser,
+  InterceptMockData, InterceptRequest, Response, closeBrowser, goto, intercept, openBrowser,
 } from 'taiko';
 
 type RespondFunction = (response: InterceptMockData) => Promise<void>;
-type MockApiHandler = (
+export type MockApiHandler = (
   respond: RespondFunction,
   request: InterceptRequest['request'],
 ) => Promise<boolean>;
@@ -15,97 +15,103 @@ type MockApiHandler = (
 jest.setTimeout(120000);
 
 beforeEach(async () => {
-  function interceptRequests(mockApiHandler: MockApiHandler) {
-    const re = /^https:\/\/localhost(\/.*)$/i;
-    const dist = resolve(__dirname, '../../../../dist');
-
-    async function tryRespondWithFile(respond: RespondFunction, path: string) {
-      let body;
-      try {
-        body = await readFile(path);
-      } catch {
-        return false;
-      }
-      const { stringify } = JSON;
-      // Oneshot override of JSON.stringify to workaround a Taiko implementation detail
-      JSON.stringify = (obj) => {
-        JSON.stringify = stringify;
-        return obj;
-      };
-      await respond({
-        body,
-        headers: {
-          'Content-Type': getType(path),
-        },
-      });
-      return true;
-    }
-
-    function staticAsset(respond: RespondFunction, url: string) {
-      const match = re.exec(url);
-      if (match == null) return Promise.resolve(false);
-      const path = match[1];
-      const normalized = normalize(path).substr(1);
-      const resolved = resolve(dist, normalized);
-      return tryRespondWithFile(respond, resolved);
-    }
-
-    function index(respond: RespondFunction) {
-      return tryRespondWithFile(respond, resolve(dist, 'index.html'));
-    }
-
-    return intercept('https://localhost/', async ({ request, respond }) => {
-      try {
-        switch (true) {
-          case mockApiHandler != null && await mockApiHandler(respond, request):
-            break;
-          case await staticAsset(respond, request.url):
-            break;
-          case await index(respond):
-            break;
-          default:
-            respond({
-              status: 404,
-            });
-        }
-      } catch (err) {
-        console.error(err);
-        respond({
-          status: 500,
-        });
-      }
-    });
-  }
-
-  const reApi = /^https:\/\/localhost\/api(\/.*)$/i;
-  const reApiRegister = /\/register/i;
-  const mockApiHandler: MockApiHandler = async (respond, request) => {
-    const { url } = request;
-    const match = reApi.exec(url);
-    if (match == null) return false;
-    const path = match[1];
-    switch (true) {
-      case Boolean(reApiRegister.exec(path)):
-        if (!request.hasPostData) await respond({ status: 400 });
-        else {
-          await respond({
-            status: 201,
-          });
-        }
-        break;
-      default:
-        await respond({
-          status: 404,
-        });
-    }
-    return true;
-  };
-
   await openBrowser();
-  await interceptRequests(mockApiHandler);
-  await goto('https://localhost/');
 });
 
 afterEach(async () => {
   await closeBrowser();
 });
+
+/**
+ * Requests that network requests to https://localhost/ be intercepted. This allows tests to run
+ * without actually starting server and database instances.
+ * @param mockApiHandler Optional /api route handler to mock API calls.
+ * @returns A promise that resolves when the browser automation tool confirms the request.
+ */
+export function interceptRequests(mockApiHandler?: MockApiHandler): Promise<void> {
+  const reRoot = /^https:\/\/localhost(\/.*)$/i;
+  const dist = resolve(__dirname, '../../../../dist');
+
+  async function tryRespondWithFile(respond: RespondFunction, path: string) {
+    let body;
+    try {
+      body = await readFile(path);
+    } catch {
+      return false;
+    }
+    const { stringify } = JSON;
+    // Oneshot override of JSON.stringify to workaround a Taiko implementation detail
+    JSON.stringify = (obj) => {
+      JSON.stringify = stringify;
+      return obj;
+    };
+    await respond({
+      body,
+      headers: {
+        'Content-Type': getType(path),
+      },
+    });
+    return true;
+  }
+
+  const reApi = /^\/api(\/.*)$/i;
+  const wrappedMockApiHandler: MockApiHandler = mockApiHandler == null
+    ? () => Promise.resolve(false)
+    : async (respond, request) => {
+      const match = reApi.exec(request.url);
+      if (match == null) return false;
+
+      const path = match[1];
+      if (!await mockApiHandler(respond, { ...request, url: path })) {
+        await respond({
+          status: 404,
+        });
+      }
+
+      return true;
+    };
+
+  function staticAsset(respond: RespondFunction, url: string) {
+    const match = reRoot.exec(url);
+    if (match == null) return Promise.resolve(false);
+    const path = match[1];
+    const normalized = normalize(path).substr(1);
+    const resolved = resolve(dist, normalized);
+    return tryRespondWithFile(respond, resolved);
+  }
+
+  function index(respond: RespondFunction) {
+    return tryRespondWithFile(respond, resolve(dist, 'index.html'));
+  }
+
+  return intercept('https://localhost/', async ({ request, respond }) => {
+    try {
+      switch (true) {
+        case await wrappedMockApiHandler(respond, request):
+          break;
+        case await staticAsset(respond, request.url):
+          break;
+        case await index(respond):
+          break;
+        default:
+          respond({
+            status: 404,
+          });
+      }
+    } catch (err) {
+      console.error(err);
+      respond({
+        status: 500,
+      });
+    }
+  });
+}
+
+/**
+ * Requests the automated browser to navigate to https://localhost/. Use {@link goto} if you wish to
+ * navigate to some other URL.
+ * @returns A promise that resolves when the browser automation tool confirms the request.
+ */
+export function gotoHome(): Promise<Response> {
+  return goto('https://localhost/');
+}
