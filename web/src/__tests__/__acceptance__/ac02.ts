f547f044f80518cc9e939157e4112b3dcf86fc3c
Shang Zhe Lee <shangzhel@student.unimelb.edu.au>
2021-10-12 11:46:12 +1100

    Add AC02: Failed registration using non-unique username.

---
+++ web/src/__tests__/__acceptance__/ac02.ts
@@ -0,0 +1,33 @@
+import {
+  button, click, focus, text, textBox, write,
+} from 'taiko';
+import { gotoHome, randomString } from './common';
+
+test('AC02: Failed registration using non-unique username', async () => {
+  expect.assertions(1);
+
+  // Precondition: User is on the registration page
+  await gotoHome();
+  await click('Register to get started');
+  const username = randomString();
+  await focus(textBox('Username'));
+  await write(username);
+  await focus(textBox('Password'));
+  await write(randomString());
+  await click(button('Register'), { waitForNavigation: false });
+  await click('Register to get started');
+
+  // Step: Enter a username that is already present in the database into the username field
+  await focus(textBox('Username'));
+  await write(username);
+
+  // Step: Enter any password into the password field
+  await focus(textBox('Password'));
+  await write(randomString());
+
+  // Step: Click register button
+  await click(button('Register'), { waitForNavigation: false });
+
+  // Pass criteria: User is shown an error dialog stating the failed registration.
+  expect(await text('Your Username has already been taken').exists()).toBe(true);
+});
