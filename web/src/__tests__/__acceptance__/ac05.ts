5cf1dc2d1dbc864a7dbebf7ff92b4de7cc1b0f4f
chomosuke <a13323600@gmail.com>
2021-10-19 14:27:06 +1100

    refactor of ac06 and ac06, tested upload image for ac06

--- web/src/__tests__/__acceptance__/ac05.ts
+++ web/src/__tests__/__acceptance__/ac05.ts
@@ -5,6 +5,10 @@ import { loginNew } from './ac03';
 import { createCard } from './ac06';
 import { randomString } from './common';
 
+export async function openDetail(name: string): Promise<void> {
+  await click(name, { force: true });
+}
+
 test('AC05: View card detail', async () => {
   expect.assertions(4);
 
@@ -12,18 +16,18 @@ test('AC05: View card detail', async () => {
 
   const names = [randomString(), randomString()];
 
-  await createCard({ name: names[0], phone: randomString(), jobTitle: randomString() });
-  await createCard({ name: names[1], phone: randomString(), jobTitle: randomString() });
+  await createCard({ name: names[0] });
+  await createCard({ name: names[1] });
 
   // Step: Select any card in the card list
-  await click(names[0], { force: true });
+  await openDetail(names[0]);
 
   // card section exist
   expect(await $('[class^=\'detailSection\']').exists()).toBe(true);
   expect(await text(names[0], toRightOf('name')).exists()).toBe(true);
 
   // Step: Select any other card in the list
-  await click(names[1], { force: true });
+  await openDetail(names[1]);
 
   // replace the panel shown
   expect(await $('[class^=\'detailSection\']').exists()).toBe(true);
--- web/src/__tests__/__acceptance__/ac06.ts
+++ web/src/__tests__/__acceptance__/ac06.ts
@@ -1,23 +1,36 @@
 import {
-  $, click, reload, text, textBox, toRightOf, write,
+  $, attach, click, fileField, reload, text, textBox, toRightOf, write,
 } from 'taiko';
+import { join } from 'path';
 import { loginNew } from './ac03';
 import { delay, randomString } from './common';
 
 export interface ICardDetails {
   name: string;
-  phone: string;
-  jobTitle: string;
+  phone?: string;
+  jobTitle?: string;
+  image?: string;
 }
 
 export async function createCard(cardDetails: ICardDetails): Promise<void> {
   // Step: User clicks the new card button
   await click('Add card');
 
+  const {
+    name, phone, jobTitle, image,
+  } = cardDetails;
+
   // Step: User enters the new details of the card
-  await write(cardDetails.name, textBox(toRightOf('name')));
-  await write(cardDetails.phone, textBox(toRightOf('phone')));
-  await write(cardDetails.jobTitle, textBox(toRightOf('job title')));
+  await write(name, textBox(toRightOf('name')));
+  if (phone != null) {
+    await write(phone, textBox(toRightOf('phone')));
+  }
+  if (jobTitle != null) {
+    await write(jobTitle, textBox(toRightOf('job title')));
+  }
+  if (image != null) {
+    await attach(join(__dirname, image), fileField({ id: 'upload' }), { force: true });
+  }
 
   // Step: User clicks button to confirms the details
   await click($('#saveButton'));
@@ -30,10 +43,10 @@ test('AC06: Successful creation of card', async () => {
   await loginNew();
 
   const name = randomString();
-  const phone = randomString();
-  const jobTitle = randomString();
 
-  await createCard({ name, phone, jobTitle });
+  await createCard({
+    name, phone: randomString(), jobTitle: randomString(), image: 'test.jpg',
+  });
 
   // cardDetail section does not exist.
   expect(await $('[class^=\'detailSection\']').exists()).toBe(false);
