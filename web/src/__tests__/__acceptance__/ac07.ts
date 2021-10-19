b738f50482c0862b339c36acc5378e26f6841940
chomosuke <a13323600@gmail.com>
2021-10-19 15:34:36 +1100

    ac07

---
+++ web/src/__tests__/__acceptance__/ac07.ts
@@ -0,0 +1,48 @@
+import {
+  $, click, reload, text, textBox, toRightOf, write,
+} from 'taiko';
+import { randomString } from './common';
+import { createCard, loginNew, openDetail } from './preconditions';
+
+test('AC07: Successful editing of card details', async () => {
+  expect.assertions(8);
+
+  await loginNew();
+
+  const name = randomString();
+  const phone = randomString();
+  const jobTitle = randomString();
+
+  await createCard({ name, image: 'test.jpg' });
+
+  await openDetail(name);
+
+  // Step: User clicks edit card button
+  await click($('#editButton'));
+
+  // Step: User changes some of the field values of the card
+  await write(phone, textBox(toRightOf('phone')));
+  await write(jobTitle, textBox(toRightOf('job title')));
+
+  // Step: User removes the image of the card
+  await click($('[class^=\'deleteImg\']'));
+  await click('Yes, Delete');
+
+  // Step: User clicks the save button
+  await click($('#saveButton'));
+
+  // card panel returns to read-only state
+  expect(await $('#saveButton').exists()).toBe(false);
+  expect(await $('#editButton').exists()).toBe(true);
+  // image no longer exist
+  expect(await $('[class^=\'noImageDiv\']').exists()).toBe(true);
+  // fields are updated
+  expect(await text(phone).exists()).toBe(true);
+  expect(await text(jobTitle).exists()).toBe(true);
+  // card updated in database
+  await reload();
+  await openDetail(name);
+  expect(await $('[class^=\'noImageDiv\']').exists()).toBe(true);
+  expect(await text(phone).exists()).toBe(true);
+  expect(await text(jobTitle).exists()).toBe(true);
+});
